9a7df9c8d3419295cf952cdc2cede0050f1c51a1
youziwing <nuthingeyt@gmail.com>
2026-06-20 19:27:07 -0400

    Update Redline.lua

--- Redline.lua
+++ Redline.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/youziwing/Redliner/refs/heads/main/AutoParry.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/youziwing/Redliner/refs/heads/main/Redliner.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/youziwing/Redliner/refs/heads/main/Hitbox.lua"))()
